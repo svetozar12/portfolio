f7ec86d9551c4bf992958a8b0b01a1b7b708e179
svetozar12 <s.gospodinov02@gmail.com>
2024-10-10 23:31:15 +0300

    basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-50 text-black font-sans">
+      <h1 className="text-2xl font-bold text-pink-500 hover:text-pink-600">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Helvetica Neue", "Arial", "sans-serif"],
+      },
+      colors: {
+        white: "var(--color-white)",
+        gray: {
+          50: "var(--color-gray-50)",
+          100: "var(--color-gray-100)",
+          500: "var(--color-gray-500)",
+          800: "var(--color-gray-800)",
+        },
+        black: "var(--color-black)",
+        pink: {
+          500: "var(--color-pink-500)",
+          600: "var(--color-pink-600)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
