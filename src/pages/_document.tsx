355e5980e385debdcf00df72551a6ecd1f143603
svetozar12 <s.gospodinov02@gmail.com>
2024-10-13 15:23:44 +0300

    seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Head>
+      <meta name="description" content="Svetozar Gospodinov Portfolio." />
+      <meta
+        name="keywords"
+        content="nextjs, seo, meta tags, freelancing, gql, fullstack"
+      />
+      <meta name="author" content="Svetozar Gospodinov" />
+      <meta property="og:title" content="Svetozar Gospodinov" />
+      <meta
+        property="og:description"
+        content="This is a description of your page."
+      />
+      <meta property="og:image" content="/path-to-your-image.jpg" />
+      <meta property="og:url" content="https://gospodinovs.com" />
+      <meta property="og:type" content="website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
-    </Html>
+    </Head>
   );
 }
